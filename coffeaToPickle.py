3a2b4b07836955be886ce30e1c0c526cf5fd5efc
mmarchegiani <mmarcheg@t3ui02.psi.ch>
2021-08-27 12:25:08 +0200

    Fit b+bb, c+cc, log(sv1mass)

--- coffeaToPickle.py
+++ coffeaToPickle.py
@@ -38,45 +38,59 @@ for isam in accumulator[next(iter(accumulator))].identifiers('dataset'):
     scaleXS[isam] = 1 if isam.startswith('BTag') else xsecs[isam]/accumulator['sumw'][isam]
 
 outputDict = {}
-for ivar in [ 'fatjet_jetproba' ]:
+for ivar in [ 'fatjet_jetproba', 'sv_logsv1mass' ]:
     for isel in [ 'msd100tau06' ]:
         for DDX in [ 'DDB', 'DDC' ]:
             for wp in [ 'M' ]:
-                for passfail in ['pass', 'fail']:
+                for (pt_low, pt_high) in [('', ''), (350, 500), (500, 'Inf')]:
+                    for passfail in ['pass', 'fail']:
 
-                    histname=f'{ivar}_{isel}{DDX}{passfail}{wp}wp'
-                    h = accumulator[histname]
-                    h.scale( scaleXS, axis='dataset' )
-                    h = h.rebin(h.fields[-1], hist.Bin(h.fields[-1], h.axis(h.fields[-1]).label, **histogram_settings['variables']['_'.join(histname.split('_')[:-1])]['binning']))
+                        if pt_low == '':
+                            histname=f'{ivar}_{isel}{DDX}{passfail}{wp}wp'
+                        else:
+                            histname=f'{ivar}_{isel}{DDX}{passfail}{wp}wpPt-{pt_low}to{pt_high}'
+                        h = accumulator[histname]
+                        h.scale( scaleXS, axis='dataset' )
+                        h = h.rebin(h.fields[-1], hist.Bin(h.fields[-1], h.axis(h.fields[-1]).label, **histogram_settings['variables']['_'.join(histname.split('_')[:-1])]['binning']))
 
-                    ##### grouping data and QCD histos
-                    datasets = [str(s) for s in h.axis('dataset').identifiers() if str(s) != 'dataset']
-                    mapping = {
-                        r'QCD ($\mu$ enriched)' : [dataset for dataset in datasets if 'QCD_Pt' in dataset],
-                        r'BTagMu': [ idata for idata in datasets if args.data in idata ],
-                    }
-                    datasets = mapping.keys()
-                    datasets_data  = [dataset for dataset in datasets if args.data in dataset]
-                    datasets_QCD = [dataset for dataset in datasets if ((args.data not in dataset) & ('GluGlu' not in dataset))]
+                        ##### grouping flavor
+                        flavors = [str(s) for s in h.axis('flavor').identifiers() if str(s) != 'flavor']
+                        mapping_flavor = {f : [f] for f in flavors}
+                        flavors_to_merge = ['bb', 'b', 'cc', 'c']
+                        for flav in flavors_to_merge:
+                            mapping_flavor.pop(flav)
+                        mapping_flavor['b_bb'] = ['b', 'bb']
+                        mapping_flavor['c_cc'] = ['c', 'cc']
+                        h = h.group("flavor", hist.Cat("flavor", "Flavor"), mapping_flavor)
 
-                    h = h.group("dataset", hist.Cat("dataset", "Dataset"), mapping)
+                        ##### grouping data and QCD histos
+                        datasets = [str(s) for s in h.axis('dataset').identifiers() if str(s) != 'dataset']
+                        mapping = {
+                            r'QCD ($\mu$ enriched)' : [dataset for dataset in datasets if 'QCD_Pt' in dataset],
+                            r'BTagMu': [ idata for idata in datasets if args.data in idata ],
+                        }
+                        datasets = mapping.keys()
+                        datasets_data  = [dataset for dataset in datasets if args.data in dataset]
+                        datasets_QCD = [dataset for dataset in datasets if ((args.data not in dataset) & ('GluGlu' not in dataset))]
+                        
+                        h = h.group("dataset", hist.Cat("dataset", "Dataset"), mapping)
 
-                    #### rescaling QCD to data
-                    dataSum = np.sum( h[args.data].sum('flavor').values()[('BTagMu',)] )
-                    QCDSum = np.sum( h[datasets_QCD].sum('dataset', 'flavor').values()[()] )
-                    QCD_rescaled = h[datasets_QCD].sum('dataset')
-                    QCD_rescaled.scale( dataSum/QCDSum )
+                        #### rescaling QCD to data
+                        dataSum = np.sum( h[args.data].sum('flavor').values()[('BTagMu',)] )
+                        QCDSum = np.sum( h[datasets_QCD].sum('dataset', 'flavor').values()[()] )
+                        QCD_rescaled = h[datasets_QCD].sum('dataset')
+                        QCD_rescaled.scale( dataSum/QCDSum )
 
-                    #### storing into dict
-                    for iflav in QCD_rescaled.values():
-                        tmpValue, sumw2 = QCD_rescaled[iflav].sum('flavor').values(sumw2=True)[()]
-                        #outputDict[ histname+'_QCD_'+iflav[0] ] = np.array( value )
-                        outputDict[ histname+'_QCD_'+iflav[0] ] = [ tmpValue, sumw2  ]
-                    tmpValue, sumw2 = h[args.data].sum('flavor').values(sumw2=True)[('BTagMu',)]
-                    outputDict[ histname+'_BtagMu' ] = [ tmpValue, sumw2 ]
+                        #### storing into dict
+                        for iflav in QCD_rescaled.values():
+                            tmpValue, sumw2 = QCD_rescaled[iflav].sum('flavor').values(sumw2=True)[()]
+                            #outputDict[ histname+'_QCD_'+iflav[0] ] = np.array( value )
+                            outputDict[ histname+'_QCD_'+iflav[0] ] = [ tmpValue, sumw2  ]
+                        tmpValue, sumw2 = h[args.data].sum('flavor').values(sumw2=True)[('BTagMu',)]
+                        outputDict[ histname+'_BtagMu' ] = [ tmpValue, sumw2 ]
 
 #### Saving into pickle
-output_dir = args.outputDir if args.outputDir else os.getcwd()+"/histograms/" + args.output + "/"
+output_dir = args.outputDir if args.outputDir else os.getcwd()+"/histograms/"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 outputFileName = output_dir + ( args.output if args.output else args.input.split('/')[-1].replace('coffea7', 'pkl')  )
--- make_SFplots.py
+++ make_SFplots.py
@@ -17,6 +17,8 @@ parser.add_argument('-o', '--outputDir', type=str, default='', help='Output dire
 #parser.add_argument('-s', '--scale', type=str, default='linear', help='Plot y-axis scale', required=False)
 #parser.add_argument('-d', '--dense', action='store_true', default=False, help='Normalized plots')
 parser.add_argument('--year', type=int, choices=[2016, 2017, 2018], help='Year of data/MC samples', required=True)
+parser.add_argument('--var', type=str, default='logsv1mass', help='Variable used in the template fit.', required=True)
+#parser.add_argument('--wpt', type=str, choices={'', 'M', 'H'}, default='', help='Pt bin')
 parser.add_argument('--data', type=str, default='BTagMu', help='Data sample name')
 parser.add_argument('--selection', type=str, default='all', help='Plot only plots with this selection. ("all" to plot all the selections in file)', required=True)
 
@@ -43,6 +45,22 @@ flavor_opts = {
     'facecolor': ['cyan', 'magenta', 'red', 'green', 'blue'],
     'edgecolor': 'black',
     'alpha': 1.0
+}
+
+flavor_normed_opts = {
+    'facecolor': 'None',
+    'edgecolor': 'green',
+    'linestyle': '-',
+    'linewidth': 2,
+    'alpha': 0.7
+}
+
+err_opts = {
+    'linestyle': '-',
+    'marker': '.',
+    'markersize': 10,
+    'color': 'k',
+    'elinewidth': 0,
 }
 
 selection = {
@@ -77,13 +95,19 @@ _mask_DDX = {
                 'M' : 0.45
             }, 
 }
+
+xlabel = {
+    'fatjet_jetproba' : r"jet Probability",
+    'sv_logsv1mass'   : r"lead. FatJet log($m_{SV,1}$/GeV)",
+}
+
 for mask_f in _final_mask:
     for DDX in _mask_DDX.keys():
         for wp, cut in _mask_DDX[DDX].items():
-            selection[f'{mask_f}{DDX}pass{wp}wp'] = selection[mask_f]
-            selection[f'{mask_f}{DDX}pass{wp}wp'] += f"{DDX}vLV2 > {str(cut)}"+"\n"
-            selection[f'{mask_f}{DDX}fail{wp}wp'] = selection[mask_f]
-            selection[f'{mask_f}{DDX}fail{wp}wp'] += f"{DDX}vLV2 < {str(cut)}"+"\n"
+            selection['{}{}pass{}wp'.format(mask_f, DDX, wp)] = selection[mask_f]
+            selection['{}{}pass{}wp'.format(mask_f, DDX, wp)] += "{}vLV2 > {}".format(DDX, cut)+"\n"
+            selection['{}{}fail{}wp'.format(mask_f, DDX, wp)] = selection[mask_f]
+            selection['{}{}fail{}wp'.format(mask_f, DDX, wp)] += "{}vLV2 < {}".format(DDX, cut)+"\n"
 """
 selection_basic = (r"$\geq$1 AK8 jets"+"\n"+
                   r"$p_T > 250 GeV$"+"\n"+
@@ -103,44 +127,89 @@ if not plot_dir.endswith('/'): plot_dir = plot_dir + '/'
 if not os.path.exists(plot_dir):
     os.makedirs(plot_dir)
 
-filename = "fitDiagnosticsTest.root"
-flavors = ['bb', 'cc', 'b', 'c', 'l']
-flavor_axis  = hist.Cat("flavor",   "Flavor")
-jetproba_axis  = hist.Bin("jetproba",  r"jet Probability", 50, 0, 2.55)
+if 'MPt' in args.input:
+    wpt = 'MPt'
+elif 'HPt' in args.input:
+    wpt = 'HPt'
+elif 'InclusivePt' in args.input:
+    wpt = 'InclusivePt'
+filename = "fitDiagnostics{wpt}.root"
 tagger = 'DDB' if 'DDB' in args.selection else 'DDC'
+flavors = ['bb', 'cc', 'b', 'c', 'l']
+#if tagger == 'DDC':
+flavor_opts['facecolor'].pop(flavors.index('b'))
+flavor_opts['facecolor'].pop(flavors.index('c')-1)
+flavors = ['b_bb', 'c_cc', 'l']
+flavor_axis  = hist.Cat("flavor",   "Flavor")
+#jetproba_axis  = hist.Bin("jetproba",  r"jet Probability", **histogram_settings['variables']['fatjet_jetproba']['binning'])
+#logsv1mass_axis  = hist.Bin("logsv1mass",  r"lead. FatJet log($m_{SV,1}$/GeV)", **histogram_settings['variables']['sv_logsv1mass']['binning'])
+observable_axis  = hist.Bin(args.var.split('_')[1],  xlabel[args.var], **histogram_settings['variables'][args.var]['binning'])
 output = {}
 
 for region in ['sfpass', 'sffail']:
     for fit in ['prefit', 'fit_s']:
-        output[f'shape_{fit}_{region}{tagger}{args.year}'] = hist.Hist("entries", flavor_axis, jetproba_axis)
+        output['shape_{}_{}{}{}'.format(fit, region, tagger, args.year)] = hist.Hist("entries", flavor_axis, observable_axis)
 
-fitdir = f'/work/mmarcheg/BTVNanoCommissioning/fitdir/{args.year}/msd100tau06{tagger}/'
+fitdir = '/work/mmarcheg/BTVNanoCommissioning/fitdir/{}/msd100tau06{}_{}/'.format(args.year, tagger, args.var.split('_')[1])
 f = uproot.open(args.input)
 for region in ['sfpass', 'sffail']:
-    fig, axes = plt.subplots(1, 2, figsize=(16,6), sharey=True)
-    #print(axes)
+    #fig, axes = plt.subplots(1, 2, figsize=(16,6), sharey=True)
     #fig, (ax1, ax2, rax1, rax2) = plt.subplots(2, 2, figsize=(16,6), sharey=True)
+    fig, axes = plt.subplots(2, 2, figsize=(12,8), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
+    fig_normed, axes_normed = plt.subplots(2, 2, figsize=(12,8), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
     #fig, axes = plt.subplots(1, 2, figsize=(16,6), sharey=True)
     for i, fit in enumerate(['prefit', 'fit_s']):
-        ax = axes[i]
+        #ax = axes[i]
         for (j, flavor) in enumerate(flavors):
-            h = f[f'shapes_{fit}/{region}/{flavor};1']
+            h = f['shapes_{}/{}/{};1'.format(fit, region, flavor)]
             weights, bins = h.to_numpy()
-            binsize = bins[1] - bins[0]
-            values = (bins - 0.5*binsize)[1:]
-            output[f'shape_{fit}_{region}{tagger}{args.year}'].fill(flavor=flavor, jetproba=values, weight=weights)
-            #ax.hist(values, bins, weights=weights, color=flavor_opts['facecolor'][::-1][j], stacked=True, label=flavor)
+            binwidth = bins[1] - bins[0]
+            weights = weights*binwidth
+            values = (bins - 0.5*binwidth)[1:]
+            #output[f'shape_{fit}_{region}{tagger}{args.year}'].fill(flavor=flavor, jetproba=values, weight=weights)
+            output['shape_{}_{}{}{}'.format(fit, region, tagger, args.year)].fill(flavor=flavor, logsv1mass=values, weight=weights)
+            axes_normed[0][i].hist(values, bins, weights=weights, edgecolor=flavor_opts['facecolor'][j], histtype='step', density=True, stacked=False, label=flavor)
 
-        plot.plot1d(output[f'shape_{fit}_{region}{tagger}{args.year}'][flavors], ax=ax, legend_opts={'loc':1}, fill_opts=flavor_opts, order=flavors, stack=True)
-        data = f[f'shapes_{fit}/{region}/data;1']
-        output[f'shape_{fit}_{region}{tagger}{args.year}'].fill(flavor=args.data, jetproba=data.values()[0], weight=data.values()[1])
-        errors = data.errors('mean')[1]
-        #plot.plot1d(output[f'shape_{fit}_{region}{tagger}{args.year}'][args.data], ax=ax, legend_opts={'loc':1}, fill_opts=data_err_opts, clear=False)
-        #plot.plot1d(output[f'shape_{fit}_{region}{tagger}{args.year}'][args.data], ax=ax, legend_opts={'loc':1}, clear=False)
-        ax.errorbar(data.values()[0], data.values()[1], yerr=errors, marker='.', linestyle='', markersize=10, elinewidth=1, color='black', label=args.data)
-        ax.set_title(f'shapes_{fit} ' + region + f' ({tagger}, {args.year})')
-        ax.legend(loc='upper right')
-        #plot.plotratio(num=output[f'shape_{fit}_{selection}{tagger}{year}']['data'].sum('flavor'), denom=output[f'shape_{fit}_{selection}{tagger}{year}'][flavors].sum('flavor'), ax=axes[i+2],
-               #unc='num')
-    histname = f'shapes_{region}{tagger}_{args.year}.png'
-    plt.savefig(plot_dir + histname, dpi=300, format="png")
+        plot.plot1d(output['shape_{}_{}{}{}'.format(fit, region, tagger, args.year)][flavors], ax=axes[0][i], legend_opts={'loc':1}, fill_opts=flavor_opts, order=flavors, stack=True)
+        #for (j, flavor) in enumerate(flavors):
+            #flavor_normed_opts['facecolor'] = flavor_opts['facecolor'][j]
+            #flavor_normed_opts['edgecolor'] = flavor_opts['facecolor'][j]
+            #err_opts['color'] = flavor_opts['facecolor'][j]
+            #plot.plot1d(output[f'shape_{fit}_{region}{tagger}{args.year}'][flavor], ax=axes_normed[0][i], density=True, legend_opts={'loc':1}, fill_opts=flavor_normed_opts, error_opts=err_opts, stack=False, clear=False)
+            #plot.plot1d(output[f'shape_{fit}_{region}{tagger}{args.year}'][flavor], ax=axes_normed[0][i], density=True, legend_opts={'loc':1}, error_opts=err_opts, stack=False, clear=False)
+        data = f['shapes_{}/{}/data;1'.format(fit, region)]
+        data_values = data.values()[0]
+        data_weights = data.values()[1]*binwidth
+        data_sum = np.sum(data_weights*binwidth)
+        data_weights_normed = data.values()[1]*binwidth/data_sum
+        errors = data.errors('mean')[1]*binwidth
+        errors_normed = data.errors('mean')[1]*binwidth/data_sum
+        #output[f'shape_{fit}_{region}{tagger}{args.year}'].fill(flavor=args.data, jetproba=data.values()[0], weight=data_weights)
+        output['shape_{}_{}{}{}'.format(fit, region, tagger, args.year)].fill(flavor=args.data, logsv1mass=data.values()[0], weight=data_weights)
+        axes[0][i].errorbar(data_values, data_weights, yerr=errors, marker='.', linestyle='', markersize=10, elinewidth=1, color='black', label=args.data)
+        axes_normed[0][i].errorbar(data_values, data_weights_normed, yerr=errors_normed, marker='.', linestyle='', markersize=10, elinewidth=1, color='black', label=args.data)
+        #plot.plot1d(output[f'shape_{fit}_{region}{tagger}{args.year}'][args.data], ax=ax'es[0][i], legend_opts={'loc':1}, error_opts=data_err_opts, clear=False)
+        axes[0][i].set_title('shapes_{} '.format(fit) + region + ' ({}, {})'.format(tagger, args.year))
+        axes_normed[0][i].set_title('shapes_{} '.format(fit) + region + ' ({}, {})'.format(tagger, args.year))
+        if i == 1:
+            axes[0][i].set_ylim(axes[0][0].get_ylim())
+            axes_normed[0][i].set_ylim(axes_normed[0][0].get_ylim())
+        axes[0][i].legend(loc='upper right')
+        axes_normed[0][i].legend(loc='upper right')
+        plot.plotratio(num=output['shape_{}_{}{}{}'.format(fit, region, tagger, args.year)][args.data].sum('flavor'),
+                       denom=output['shape_{}_{}{}{}'.format(fit, region, tagger, args.year)][flavors].sum('flavor'),
+                       ax=axes[1][i],
+                       error_opts=data_err_opts, denom_fill_opts={}, guide_opts={},
+                       unc='num')
+        plot.plotratio(num=output['shape_{}_{}{}{}'.format(fit, region, tagger, args.year)][args.data].sum('flavor'),
+                       denom=output['shape_{}_{}{}{}'.format(fit, region, tagger, args.year)][flavors].sum('flavor'),
+                       ax=axes_normed[1][i],
+                       error_opts=data_err_opts, denom_fill_opts={}, guide_opts={},
+                       unc='num')
+        axes[1][i].set_xlim(**histogram_settings['variables']['sv_logsv1mass']['xlim'])
+        axes_normed[1][i].set_xlim(**histogram_settings['variables']['sv_logsv1mass']['xlim'])
+        axes[1][i].set_ylim(0.5, 1.5)
+        axes_normed[1][i].set_ylim(0.5, 1.5)
+    histname = 'shapes_{}{}{}_{}.png'.format(region, tagger, wpt, args.year)
+    fig.savefig(plot_dir + histname, dpi=300, format="png")
+    fig_normed.savefig(plot_dir + histname.replace('.png', '_normed.png'), dpi=300, format="png")
