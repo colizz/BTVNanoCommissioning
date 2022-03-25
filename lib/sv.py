c21c92c9484d6266be93e890db66afe50d329089
mmarchegiani <mmarcheg@t3ui02.psi.ch>
2022-03-25 11:15:00 +0100

    New lib scheme

---
+++ lib/sv.py
@@ -0,0 +1,17 @@
+import collections
+import numpy as np
+import awkward as ak
+
+def get_nsv(sj, sv, R=0.4):
+
+    sv_dr = sj.delta_r(sv)
+    nsv = ak.count(sv_dr[sv_dr < R], axis=1)
+
+    return nsv
+
+def get_sv_in_jet(jet, sv, R=0.8):
+
+    sv_dr = jet.delta_r(sv)
+    sv_in_jet = sv_dr < R
+
+    return sv_in_jet
---
+++ scripts/dataset/build_dataset.py
@@ -0,0 +1,18 @@
+import os
+import argparse
+
+from utils2.Configurator import Configurator
+from utils2.Dataset import Dataset
+
+parser = argparse.ArgumentParser(description='Build dataset file in json format')
+parser.add_argument('--cfg', default=os.getcwd() + "/config/test.py", help='Config file with parameters specific to the current run', required=False)
+parser.add_argument('-d', '--download', action='store_true', default=False, help='Download dataset files on local machine', required=False)
+
+args = parser.parse_args()
+config = Configurator(args.cfg, create_dataset=True)
+
+dataset = Dataset(config.dataset, config.storage_prefix, config.json)
+dataset.save()
+
+if args.download:
+    dataset.download()
