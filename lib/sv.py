d55bf4a7bf43d436aa091cea5d43256b58efc121
mmarchegiani <matmarcheg@gmail.com>
2022-07-01 14:01:33 +0200

    Fix SV bug

--- lib/sv.py
+++ lib/sv.py
@@ -4,14 +4,14 @@ import awkward as ak
 
 def get_nsv(sj, sv, R=0.4):
 
-    sv_dr = sj.delta_r(sv)
+    sv_dr = sj.delta_r(sv.p4)
     nsv = ak.count(sv_dr[sv_dr < R], axis=1)
 
     return nsv
 
 def get_sv_in_jet(jet, sv, R=0.8):
 
-    sv_dr = jet.delta_r(sv)
+    sv_dr = jet.delta_r(sv.p4)
     sv_in_jet = sv_dr < R
 
     return sv_in_jet
