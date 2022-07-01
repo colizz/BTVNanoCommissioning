66ae2afc96184950bcdfa58d1c12837454f41f67
mmarchegiani <matmarcheg@gmail.com>
2022-07-01 16:06:14 +0200

    Config for pt reweighting

---
+++ config/pt_reweighting_2018.py
@@ -0,0 +1,43 @@
+cfg =  {
+    # Dataset parameters
+    "dataset"  : "datasets/DAS/datasets_btag2018EOY.txt",
+    "json"     : "datasets/RunIIAutumn18EOY18.json",
+    "storage_prefix" : "/pnfs/psi.ch/cms/trivcat/store/user/mmarcheg/BTVNanoCommissioning",
+    "campaign" : "EOY",
+    "year"     : "2018",
+
+    # PU files
+    "puFile"   : "correction_files/PileupHistogram-goldenJSON-13tev-2018-69200ub-99bins.root",
+    "nTrueFile": "correction_files/nTrueInt_RunIIAutumn18EOY18_local_2018.coffea",
+
+    # JEC
+    "JECfolder": "correction_files/tmp",
+
+    # Input and output files
+    "workflow" : "pt_reweighting",
+    "input"    : "datasets/RunIIAutumn18EOY18_local.json",
+    "output"   : "histograms/pt_reweighting_2018.coffea",
+    "plots"    : "plots/pt_reweighting_2018",
+    "output_reweighting" : "correction_files/pt_reweighting/pt_reweighting_2018",
+
+    # Executor parameters
+    "run_options" : {
+        "executor"       : "parsl/slurm",
+        "workers"        : 12,
+        "scaleout"       : 10,
+        "partition"      : "standard",
+        "walltime"       : "12:00:00",
+        "mem_per_worker" : None, # GB
+        "exclusive"      : True,
+        "chunk"          : 50000,
+        "max"            : None,
+        "skipbadfiles"   : None,
+        "voms"           : None,
+        "limit"          : None,
+    },
+
+    # Processor parameters
+    "checkOverlap" : False,
+    "hist2d"       : False,
+    "mupt"         : 5,
+}
