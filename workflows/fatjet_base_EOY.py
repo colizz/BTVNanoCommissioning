444af8817c00896d70b1ca3d5bc6be7899fe5dea
mmarchegiani <matmarcheg@gmail.com>
2022-07-18 09:41:18 +0200

    Workflow for EOY

---
+++ workflows/fatjet_base_EOY.py
@@ -0,0 +1,69 @@
+import os
+
+import numpy as np
+import coffea
+from coffea import hist, processor, lookup_tools
+from coffea.lookup_tools.dense_lookup import dense_lookup
+from coffea.util import save, load
+
+from workflows.fatjet_base import fatjetBaseProcessor
+from parameters import triggers_EOY, JECversions_EOY, JERversions_EOY, jecTarFiles, FinalMask, PtBinning, AK8Taggers, AK8TaggerWP
+
+class fatjetEOYProcessor(fatjetBaseProcessor):    
+    # Function to load year-dependent parameters
+    def load_metadata(self):
+        self._dataset = self.events.metadata["dataset"]
+        self._sample = self.events.metadata["sample"]
+        self._year = self.events.metadata["year"]
+        self._triggers = triggers_EOY[self.cfg.finalstate][self._year]
+        self._btag = btag[self._year]
+        self._isMC = 'genWeight' in self.events.fields
+        # JEC
+        self._JECversion = JECversions_EOY[self._year]['MC' if self._isMC else 'Data']
+        self._JERversion = JERversions_EOY[self._year]['MC' if self._isMC else 'Data']
+        self._goldenJSON = goldenJSON[self._year]
+
+    def applyJERC( self, typeJet ):
+        '''Based on https://coffeateam.github.io/coffea/notebooks/applying_corrections.html#Applying-energy-scale-transformations-to-Jets'''
+
+        JECversion = self._JECversion
+        jets = self.events[typeJet]
+        fixedGridRhoFastjetAll = self.events.fixedGridRhoFastjetAll
+        events_cache = self.events.caches[0]
+        isData = not self._isMC
+        JECversion = self._JECversion
+
+        ext = lookup_tools.extractor()
+        JECtypes = [ 'L1FastJet', 'L2Relative', 'L2Residual', 'L3Absolute', 'L2L3Residual' ]
+        jec_stack_names = [ JECversion+'_'+k+'_'+typeJet for k in JECtypes ]
+        JECtypesfiles = [ '* * '+self.corrJECfolder+'/'+k+'.txt' for k in jec_stack_names ]
+        ext.add_weight_sets( JECtypesfiles )
+        ext.finalize()
+        evaluator = ext.make_evaluator()
+
+        jec_inputs = {name: evaluator[name] for name in jec_stack_names}
+        corrector = FactorizedJetCorrector( **jec_inputs )
+        # for i in jec_inputs: print(i,'\n',evaluator[i])
+
+        #print(dir(evaluator))
+        #print()
+        jec_stack = JECStack(jec_inputs)
+        name_map = jec_stack.blank_name_map
+        name_map['JetPt'] = 'pt'
+        name_map['JetMass'] = 'mass'
+        name_map['JetEta'] = 'eta'
+        name_map['JetA'] = 'area'
+
+        jets['pt_raw'] = (1 - jets['rawFactor']) * jets['pt']
+        jets['mass_raw'] = (1 - jets['rawFactor']) * jets['mass']
+        jets['rho'] = ak.broadcast_arrays(fixedGridRhoFastjetAll, jets.pt)[0]
+        name_map['ptRaw'] = 'pt_raw'
+        name_map['massRaw'] = 'mass_raw'
+        name_map['Rho'] = 'rho'
+        if not isData:
+            jets['pt_gen'] = ak.values_astype(ak.fill_none(jets.matched_gen.pt, 0), np.float32)
+            name_map['ptGenJet'] = 'pt_gen'
+
+        jet_factory = CorrectedJetsFactory(name_map, jec_stack)
+        corrected_jets = jet_factory.build(jets, lazy_cache=events_cache)
+    
