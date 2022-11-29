7b8fac5fa94338ad99fe987b3c62f1e02b6c1a06
mmarchegiani <matmarcheg@gmail.com>
2022-11-29 19:07:32 +0100

    File with fit parameters

---
+++ parameters_fit.py
@@ -0,0 +1,9 @@
+from parameters import PtBinning, AK8TaggerWP
+
+fit_parameters ={
+    "baseline" : {
+        "l"  : {"value" : 1, "lo" : 0, "hi" : 2},
+        "b+bb" : {"value" : 1, "lo" : 0, "hi" : 2},
+        "c+cc" : {"value" : 1, "lo" : 0, "hi" : 2}
+    },
+}
