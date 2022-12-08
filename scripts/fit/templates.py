19089b890af0db5194cbade1c40d252eb910da50
mmarchegiani <matmarcheg@gmail.com>
2022-12-08 18:01:58 +0100

    Update templates script

--- scripts/fit/templates.py
+++ scripts/fit/templates.py
@@ -11,28 +11,31 @@ import itertools
 import os
 import pickle
 
-from pocket_coffea.utils.configurator import Configurator
 from pocket_coffea.utils.plot_utils import dense_axes, get_axis_items
 
 parser = argparse.ArgumentParser(description='Save histograms in pickle format for combine fit')
-parser.add_argument('--cfg', default=os.getcwd() + "/config/test.json", help='Config file with parameters specific to the current run', required=False)
-parser.add_argument('-v', '--version', type=str, default=None, help='Version of output (e.g. `v01`, `v02`, etc.)')
-parser.add_argument('-l', '--label', type=str, default=None, help='Extra label for output file')
+parser.add_argument('-i', '--input', type=str, help='Input file with histograms', required=True)
+parser.add_argument('-o', '--output', type=str, default=None, help='Output folder to save templates', required=False)
+parser.add_argument('-l', '--label', type=str, default=None, help='Extra label for output file', required=False)
 
 args = parser.parse_args()
-config = Configurator(args.cfg, plot=True, plot_version=args.version)
 
-if os.path.isfile( config.outfile ): accumulator = load(config.outfile)
-else: sys.exit(f"Input file '{config.outfile}' does not exist")
+if os.path.isfile( args.input ): accumulator = load(args.input)
+else: sys.exit(f"Input file '{args.input}' does not exist")
+
+if args.output == None:
+    args.output = os.path.dirname(args.input)
+    if args.output == '':
+        args.output = os.getcwd()
 
 flavors = {'l', 'c', 'b', 'cc', 'bb'}
 output_templates = {}
-fit_variables = [ 'events_logsummass',  'events_logsv1mass', 'events_logsumcorrmass' ]
+fit_variables = [ 'events_logsummass_1', 'events_logsv1mass_1', 'events_logsumcorrmass_1' ]
 
 for histname in fit_variables:
-    h_5f = accumulator['variables'][histname]
-    #print(h_5f)
-    #print(h_5f.keys())
+    h = accumulator['variables'][histname]
+    h_5f = { f : sum({k : val for k, val in h.items() if k.endswith(f"_{f}")}.values()) for f in flavors }
+    h_5f.update({'DATA' : h['DATA']})
     h_3f = {'l' : h_5f['l'], 'b+bb' : h_5f['b'] + h_5f['bb'], 'c+cc' : h_5f['c'] + h_5f['cc'], 'DATA' : h_5f['DATA']}
     for scheme, h in zip(['3f', '5f'], [h_3f, h_5f]):
         print(f"Histogram: {histname}\tScheme: {scheme}")
@@ -61,28 +64,16 @@ for histname in fit_variables:
                         output_templates[scheme][f"{histname}_{year}_{cat}_QCD_{f}_{var}"] = [sumw, sumw2]
 
 #### Saving into pickle
-output_dir = config.output
-if not os.path.exists(config.output):
-    os.makedirs(config.output)
+if not os.path.exists(args.output):
+    os.makedirs(args.output)
 
 for scheme, templates in output_templates.items():
-    filename = config.outfile.replace('.coffea', f'_templates_{scheme}.pkl')
+    filename = args.input.replace('.coffea', f'_templates_{scheme}.pkl')
 
     if args.label:
         filename = filename.replace('.pkl', f'_{args.label}.pkl')
-    print(f"Saving config file to {filename}")
-    outfile = open( filename, 'wb' )
+    filepath = os.path.join(args.output, filename)
+    print(f"Saving templates file to {filepath}")
+    outfile = open( filepath, 'wb' )
     pickle.dump( templates, outfile, protocol=2 )
     outfile.close()
-
-    """
-    key = 'hist_leadfatjet_pt'
-    if not key in accumulator.keys():
-        raise NotImplementedError
-    h = accumulator[key]
-    cats = [str(s) for s in h.axis('cat').identifiers() if str(s) != 'cat']
-    categoriesFileName = '/'.join( config.outfile.split('/')[:-1] ) + '/categories.json'
-    print("Saving categories file to " + categoriesFileName)
-    categoriesFile = open( categoriesFileName, 'w' )
-    json.dump(cats, categoriesFile, indent=1)
-    """
