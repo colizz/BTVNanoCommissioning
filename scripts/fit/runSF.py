45e6b2753312aa46c2aa1d592e12508760d786c3
mmarchegiani <matmarcheg@gmail.com>
2022-12-13 14:36:45 +0100

    SF script in many modes

--- scripts/fit/runSF.py
+++ scripts/fit/runSF.py
@@ -3,8 +3,6 @@ import sys
 import argparse
 import json
 
-import numpy as np
-
 sys.path.append('/work/mmarcheg/BTVNanoCommissioning')
 
 from utils.Fit import Fit
@@ -13,19 +11,25 @@ from parameters import categories
 parser = argparse.ArgumentParser(description='Save histograms in pickle format for combine fit')
 parser.add_argument('-i', '--input', default=None, help='Input templates', required=True)
 parser.add_argument('-o', '--output', default="/work/mmarcheg/BTVNanoCommissioning/output/fit", help='Output folder', required=True)
-parser.add_argument('--only', type=str, default=None, help='Filter categories by key', required=True)
+parser.add_argument('--only', type=str, default=None, help='Filter categories by key', required=False)
 parser.add_argument('--scheme', type=str, choices=['3f', '5f'],  help='3-flavor scheme', required=True)
-parser.add_argument('--binwidth', type=float, default=0.2, choices=[0.1, 0.2, 0.4],  help='Specify the binwidth of the logsumcorrmass distribution', required=False)
-parser.add_argument('--year', type=str, choices=["2016_PreVFP", "2016_PostVFP", "2017", "2018"],  help='Specify the data-taking year', required=True)
-
+parser.add_argument('--binwidth', type=float, default=0.4, choices=[0.1, 0.2, 0.4],  help='Specify the binwidth of the logsumcorrmass distribution', required=False)
+parser.add_argument('--year', type=str, choices=["2016_PreVFP", "2016_PostVFP", "2017", "2018"], help='Specify the data-taking year', required=True)
+parser.add_argument('-m', '--mode', type=str, default="FitDiagnostics", choices=["FitDiagnostics", "MultiDimFit", "all"], help='Specify combine mode', required=False)
 args = parser.parse_args()
 
 if args.only:
-    categories = list(filter(lambda cat : all(f in cat for f in [args.only]), categories))
+    args.only = args.only.split('*')
+    categories = list(filter(lambda cat : all(f in cat for f in args.only), categories))
 
 if os.path.exists(args.output):
     sys.exit("The output folder {} is already existing. Please choose a different folder name.".format(args.output))
 
+failed_fits = []
 for var in ['events_logsumcorrmass_1']:
-    fit = Fit(args.input, args.output, categories, var, args.year, xlim=(-1.0, 5.0), binwidth=args.binwidth, scheme=args.scheme)
-    fit.run_fits()
+    fit = Fit(args.input, args.output, categories, var, args.year, xlim=(-1.2, 4.8), binwidth=args.binwidth, scheme=args.scheme)
+    if args.mode == "all":
+        fit.run_fits("FitDiagnostics")
+        fit.run_fits("MultiDimFit")
+    else:
+        fit.run_fits(args.mode)
